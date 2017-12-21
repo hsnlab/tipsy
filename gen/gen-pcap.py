f083722687ac0cd10b8e2da89d21190fb8c55321
Tamas Levai <levait@tmit.bme.hu>
2017-12-21 12:09:22 +0100

    Move and extend mgw-gen-pcap.py to gen-pcap.py
    TODO: implement proper bng pcap gen

--- gen/gen-pcap.py
+++ gen/gen-pcap.py
@@ -32,45 +32,92 @@ class ObjectView(object):
         return self.__dict__.__repr__()
 
 
-def downlink(params_tuple):
-    (pkt_num, pkt_size, conf) = params_tuple
+def gen_packets(params_tuple):
+    (dir, pkt_num, pkt_size, conf) = params_tuple
+    pl = conf.pipeline
     pkts = []
-    for i in range(pkt_num):
-        server = random.choice(conf.srvs)
-        user = random.choice(conf.users)
-        proto = random.choice([TCP(), UDP()])
-        p = (
-            Ether(dst=conf.gw.mac) /
-            IP(src=server.ip, dst=user.ip) /
-            proto
-        )
-        p = add_payload(p, pkt_size)
-        p = vwrap(p, conf)
-        pkts.append(PicklablePacket(p))
-    return pkts
+    if dir[0] == b:
+        dir = ('dl', 'ul')
+    else:
+        dir = (dir, dir)
+    for i in range(pkt_num / 2 + 1):
+        for d in dir:
+            pkt_gen_func = getattr(sys.modules[__name__],
+                                   '_gen_%s_pkt_%s' % (d, pl))
+            p = pkt_gen_func(pkt_size, conf)
+            pkts.append(PicklablePacket(p))
+    return pkts[:pkt_num]
 
 
-def uplink(params_tuple):
-    (pkt_num, pkt_size, conf) = params_tuple
-    pkts = []
-    for i in range(pkt_num):
-        server = random.choice(conf.srvs)
-        user = random.choice(conf.users)
-        proto = random.choice([TCP(), UDP()])
-        bst = conf.bsts[user.bst]
-        p = (
-            Ether(src=bst.mac, dst=conf.gw.mac, type=0x0800) /
-            IP(src=bst.ip, dst=conf.gw.ip) /
-            UDP(sport=4789, dport=4789) /
-            VXLAN(vni=user.teid, flags=0x08) /
-            Ether(dst=conf.gw.mac, type=0x0800) /
-            IP(src=user.ip, dst=server.ip) /
-            proto
-        )
-        p = add_payload(p, pkt_size)
-        p = vwrap(p, conf)
-        pkts.append(PicklablePacket(p))
-    return pkts
+def _gen_dl_pkt_mgw(pkt_size, conf):
+    server = random.choice(conf.srvs)
+    user = random.choice(conf.users)
+    proto = random.choice([TCP(), UDP()])
+    p = (
+        Ether(dst=conf.gw.mac) /
+        IP(src=server.ip, dst=user.ip) /
+        proto
+    )
+    p = add_payload(p, pkt_size)
+    return p
+
+
+def _gen_dl_pkt_vmgw(pkt_size, conf):
+    p = _gen_dl_pkt_mgw(pkt_size, conf)
+    p = vwrap(p, conf)
+    return p
+
+
+def _gen_dl_pkt_bng(pkt_size, conf):
+    server = random.choice(conf.srvs)
+    user = random.choice(conf.users)
+    proto = random.choice([TCP(), UDP()])
+    p = (
+        Ether(dst=conf.gw.mac) /
+        IP(src=server.ip, dst=user.ip) /
+        proto
+    )
+    p = add_payload(p, pkt_size)
+    return p
+
+
+def _gen_ul_pkt_mgw(pkt_size, conf):
+    server = random.choice(conf.srvs)
+    user = random.choice(conf.users)
+    proto = random.choice([TCP(), UDP()])
+    p = (
+        Ether(src=bst.mac, dst=conf.gw.mac, type=0x0800) /
+        IP(src=bst.ip, dst=conf.gw.ip) /
+        UDP(sport=4789, dport=4789) /
+        VXLAN(vni=user.teid, flags=0x08) /
+        Ether(dst=conf.gw.mac, type=0x0800) /
+        IP(src=user.ip, dst=server.ip) /
+        proto
+    )
+    p = add_payload(p, pkt_size)
+    return p
+
+
+def _gen_ul_pkt_vmgw(pkt_size, conf):
+    p = _gen_ul_pkt_mgw(pkt_size, conf)
+    p = vwrap(p, conf)
+    return p
+
+
+def _gen_ul_pkt_bng(pkt_size, conf):
+    proto = random.choice([TCP(), UDP()])
+    cpe = conf.cpe[user.tun_end_id]
+    p = (
+        Ether(src=cpe.mac, dst=conf.gw.mac, type=0x0800) /
+        IP(src=cpe.ip, dst=conf.gw.ip) /
+        UDP(sport=4789, dport=4789) /
+        VXLAN(vni=user.teid, flags=0x08) /
+        Ether(dst=conf.gw.mac, type=0x0800) /
+        IP(src=user.ip, dst=server.ip) /
+        proto
+    )
+    p = add_payload(p, pkt_size)
+    return p
 
 
 def add_payload(p, pkt_size):
@@ -83,8 +130,6 @@ def add_payload(p, pkt_size):
 
 def vwrap(pkt, conf):
     'Add VXLAN header for infra processing in case of a virtual mgw.'
-    if not conf.virtual_mgw:
-        return pkt
     vxlanpkt = (
         Ether(src=conf.dcgw.mac, dst=conf.gw.mac) /
         IP(src=conf.dcgw.ip, dst=conf.gw.ip) /
@@ -131,33 +176,21 @@ if args.json:
 def conv_fn(d): return ObjectView(**d)
 conf = json.load(args.conf, object_hook=conv_fn)
 
+dir = '%sl' % args.dir[0]
 wargs = []
 worker_num = min(args.pkt_num, args.thread)
 pkt_left = args.pkt_num
 ppw = args.pkt_num / worker_num
 for _ in range(worker_num):
-    wargs.append((ppw, args.pkt_size, conf))
+    wargs.append((dir, ppw, args.pkt_size, conf))
     pkt_left -= ppw
 if pkt_left > 0:
-    wargs.append((args.pkt_num % worker_num, args.pkt_size, conf))
+    wargs.append((dir, args.pkt_num % worker_num, args.pkt_size, conf))
     worker_num += 1
 workers = multiprocessing.Pool(worker_num)
 
-if args.dir == 'uplink' or args.dir == 'u':
-    pkts = workers.map(uplink, wargs)
-    pkts = [p for wpkts in pkts for p in wpkts]  # flatten pkts
-elif args.dir == 'downlink' or args.dir == 'd':
-    pkts = workers.map(downlink, wargs)
-    pkts = [p for wpkts in pkts for p in wpkts]
-elif args.dir == 'bidir' or args.dir == 'b':
-    upkts = workers.map(uplink, wargs)
-    dpkts = workers.map(downlink, wargs)
-    upkts = [p for wpkts in upkts for p in wpkts]
-    dpkts = [p for wpkts in dpkts for p in wpkts]
-    pkts = list(itertools.chain(*zip(upkts, dpkts)))[:args.pkt_num]
-else:
-    raise RuntimeError("Unknown direction: %s" % args.dir)
-
+pkts = workers.map(gen_packets, wargs)
+pkts = [p for wpkts in pkts for p in wpkts]
 pkts = map(PicklablePacket.__call__, pkts)
 
 if args.ascii:
