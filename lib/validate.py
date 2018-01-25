820011c45911e5868e45a58186bdecdbfa7544df
Felician Nemeth <nemethf@tmit.bme.hu>
2018-01-25 18:56:04 +0100

    Add copyright boilerplate to source files

--- lib/validate.py
+++ lib/validate.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
+
+# TIPSY: Telco pIPeline benchmarking SYstem
+#
+# Copyright (C) 2018 by it's authors (See AUTHORS)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# This file incorporates work covered by the following copyright and
+# permission notice:
+#
 # Copyright (c) 2013 Julian Berman
-# Copyright (c) 2018 Felician Nemeth
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
