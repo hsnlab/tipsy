1e73bdb0208b3d8001c6e2fa5ca65f7f872a6da1
Felician Nemeth <nemethf@tmit.bme.hu>
2018-05-31 14:01:44 +0200

    vagrant, tester: Update dependencies

--- vagrant/tester/install.sh
+++ vagrant/tester/install.sh
@@ -17,13 +17,14 @@ apt-get install --yes \
     python-pip \
     python-dev \
     python3-jsonschema \
-    python-matplotlib \
+    python3-matplotlib \
     libffi-dev \
     libssl-dev \
     libxml2-dev \
     libxslt1-dev \
     zlib1g-dev \
     texlive-latex-base \
+    texlive-pictures \
     scapy
 
 echo 192.168.53.3 sut.local >> /etc/hosts
